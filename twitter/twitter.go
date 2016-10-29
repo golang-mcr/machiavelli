d104eeb2b1876f8464097d2fcb4521acac7a7173
danbondd <danbondd@gmail.com>
2016-10-29 18:16:33 +0100

    Implement send tweet

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -1,6 +1,10 @@
 package twitter
 
-import "net/http"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
 
 // Tweet holds the contents of a tweet
 type Tweet struct {
@@ -10,11 +14,12 @@ type Tweet struct {
 // Client defines the opertations of twitter client
 type Client interface {
 	Listen(search string) (<-chan Tweet, func())
-	Tweet(tweet Tweet) error
+	Tweet(tweet string) error
 }
 
 type client struct {
 	httpClient *http.Client
+	config     *Config
 }
 
 func (c client) Listen(search string) (<-chan Tweet, func()) {
@@ -23,12 +28,34 @@ func (c client) Listen(search string) (<-chan Tweet, func()) {
 	return ch, func() {}
 }
 
-func (c client) Tweet(tweet Tweet) error {
+func (c client) Tweet(tweet string) error {
+	if len(tweet) > 140 {
+		return errors.New("tweet exceeds 140 character limit")
+	}
+
+	oa := NewOAuthDetails(c.config, tweet)
+
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf(apiURL+apiVersion+statusURI+"?status=%s", encodeStatus(&tweet)), nil)
+	if err != nil {
+		return fmt.Errorf("error building request: %v", err)
+	}
+	req.Header.Set(authHeader, fmt.Sprintf("%s", oa))
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("%s", res.Status)
+	}
+
 	return nil
 }
 
 // NewClient takes a http client with oauth credentials
 // to make future calls to twitter
-func NewClient(httpClient *http.Client) Client {
-	return client{httpClient}
+func NewClient(httpClient *http.Client, config *Config) Client {
+	return client{httpClient, config}
 }
