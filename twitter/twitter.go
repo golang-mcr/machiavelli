4bc270347036663fc9f9b08c38325aa221df2b26
Tim Blackwell <timmblackwell@gmail.com>
2016-10-29 14:54:19 +0100

    added constructor to twitter

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -1,5 +1,7 @@
 package twitter
 
+import "net/http"
+
 // Tweet holds the contents of a tweet
 type Tweet struct {
 }
@@ -8,4 +10,13 @@ type Tweet struct {
 type Client interface {
 	Listen(search string) (<-chan Tweet, func())
 	Tweet(tweet Tweet) error
+}
+
+type client struct {
+	httpClient *http.Client
+}
+
+// NewClient takes a http client with oauth credentials
+// to make future calls to twitter
+func NewClient(httpClient *http.Client) *Client {
 }
