11370e96106ec169395a5dee9a748fd698733559
danbondd <danbondd@gmail.com>
2016-10-29 15:46:46 +0100

    Implement Client

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -17,7 +17,18 @@ type client struct {
 	httpClient *http.Client
 }
 
+func (c client) Listen(search string) (<-chan Tweet, func()) {
+	ch := make(chan Tweet)
+
+	return ch, func() {}
+}
+
+func (c client) Tweet(tweet Tweet) error {
+	return nil
+}
+
 // NewClient takes a http client with oauth credentials
 // to make future calls to twitter
-func NewClient(httpClient *http.Client) *Client {
+func NewClient(httpClient *http.Client) Client {
+	return client{httpClient}
 }
