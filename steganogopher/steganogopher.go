f09be6d23bf9135af2e7fd1563efadd4d2964ebd
Edoardo Paolo Scalafiotti <edoardo849@gmail.com>
2016-10-29 15:00:29 +0100

    Modifications to the interface

--- steganogopher/steganogopher.go
+++ steganogopher/steganogopher.go
@@ -1,6 +1,7 @@
 package steganogopher
 
-type Crypter interface {
-	Encode(interface{}, interface{}) interface{}
-	Decode(interface{}) interface{}
-}
+// Encoder is the signature of the Encode function. Specs TBD
+type Encoder func(interface{}, interface{}) interface{}
+
+// Decoder is the signature of the Decode function. Specs TBD.
+type Decoder func(interface{}, interface{}) interface{}
